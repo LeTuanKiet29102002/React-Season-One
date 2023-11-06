255993ef2750c1b08ad3573db7adb3d6bd6cc917
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-06 16:44:33 +0700

    conditional-output-react

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,36 +1,30 @@
 import React from "react";
 
-// class ChildComponent extends React.Component {
+class ChildComponent extends React.Component {
+
+    state = {
+        showJob: false
+    }
+
+    handleshowJob =()=>{
+        this.setState({
+            showJob: !this.state.showJob
+        })
+    }
 
 
 
-
-//     render() {
-//         let { name, age, abc } = this.props;
-//         console.log("check props: ", this.props);
-//         return (
-//             <React.Fragment>
-//                 Child Component:{name} - {age}
-//                 <div className="job-list">
-//                     {
-//                         abc.map((item, index) => {
-//                             return (
-//                                 <div key={item.id}>
-//                                     {item.title}-{item.salary}
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
-//             </React.Fragment>
-//         );
-//     }
-// }
-
-const ChildComponent =(props)=>{
-    let { abc} = props;
-        console.log("check props: ", props);
+    render() {
+        let { name, age, abc } = this.props;
+        let showJob = this.state.showJob;
+        console.log("check props: ", this.props);
         return (
+            <React.Fragment>
+                {/* Child Component:{name} - {age} */}
+                {showJob===false ?
+                <div><button onClick={()=>{this.handleshowJob()}}>Show</button></div>
+                :
+                <>
                 <div className="job-list">
                     {
                         abc.map((item, index) => {
@@ -42,7 +36,30 @@ const ChildComponent =(props)=>{
                         })
                     }
                 </div>
-        )
+                <div><button onClick={()=>{this.handleshowJob()}}>Hide</button></div>
+                </>
+                }
+            </React.Fragment>
+        );
+    }
 }
+
+// const ChildComponent =(props)=>{
+//     let { abc} = props;
+//         console.log("check props: ", props);
+//         return (
+//                 <div className="job-list">
+//                     {
+//                         abc.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title}-{item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//         )
+// }
 
 export default ChildComponent;

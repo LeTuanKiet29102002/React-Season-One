45b76730111a19c2a3618eb2230d7cd50177f7fe
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-08 09:54:26 +0700

    Todo-App-React-02

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -1,35 +1,88 @@
 import React from 'react';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { Button, Tooltip } from "antd";
 import './ListTodo.scss'
 import AddJobTodo from './AddJobTodo';
 import { toast } from 'react-toastify';
+import TableJobTodo from './TableJobTodo';
 
 
 class ListTodo extends React.Component {
     state = {
         listTodos: [
-            { id: 'todo1', title: 'doing homework' ,salary:1000},
-            { id: 'todo2', title: 'watching tivi' ,salary:2000},
-            { id: 'todo3', title: 'learning English' ,salary:3000},
-        ]
+            { id: 'todo1', title: 'doing homework', salary: 1000 },
+            { id: 'todo2', title: 'watching tivi', salary: 2000 },
+            { id: 'todo3', title: 'learning English', salary: 3000 },
+        ],
+        editTodobtn: {}
+
     }
 
-    AddNewTodo = (todo)=>{
+    AddNewTodo = (todo) => {
         this.setState({
-            listTodos: [...this.state.listTodos,todo]
+            listTodos: [...this.state.listTodos, todo]
         })
         toast.success("Wow so easy!")
     }
+    DeleteTodo = (todo) => {
+        let currentTodo = this.state.listTodos;
+        currentTodo = currentTodo.filter(item => item.id !== todo.id);
+        this.setState({
+            listTodos: currentTodo
+        })
+    }
+    EditTodo = (todo) => {
+        let { editTodobtn, listTodos } = this.state;
+        let isEmptyObj = Object.keys(editTodobtn).length === 0;
+
+        //save
+        if (isEmptyObj === false && editTodobtn.id === todo.id) {
+            let listTodoCopy = [...listTodos];
+            //Find index of specific object using findIndex method.    
+            let objIndex = listTodoCopy.findIndex((item => item.id === todo.id));
+
+            //Update object's name property.
+            listTodoCopy[objIndex].title = editTodobtn.title
+            listTodoCopy[objIndex].salary = editTodobtn.salary
+
+            this.setState({
+                listTodos: listTodoCopy,
+                editTodobtn: {}
+            })
+            toast.success('Update successfully!')
+
+        }
+        //edit
+        else {
+            this.setState({
+                editTodobtn: todo
+            })
+        }
+
+    }
+
+    // Hàm cập nhật editTodobtn từ TableJobTodo (file con)
+    UpdateEditTodoBtn = (editedTodo) => {
+
+        this.setState({
+            editTodobtn: editedTodo
+        });
+    }
+
     render() {
-        let { listTodos } = this.state;
+        // let { listTodos } = this.state;
         // let listTodos = this.state.listTodos
         return (
             <div className='list-todo-container'>
                 <AddJobTodo
-                    AddNewTodo = {this.AddNewTodo}
+                    AddNewTodo={this.AddNewTodo}
                 />
-                <div className='list-todo-content'>
+                <TableJobTodo
+                    ArrayJobTodo={this.state.listTodos}
+                    editTodobtn={this.state.editTodobtn}
+                    DeleteTodo={this.DeleteTodo}
+                    EditTodo={this.EditTodo}
+                    UpdateEditTodoBtn={this.UpdateEditTodoBtn}
+                />
+                {/* <div className='list-todo-content'>
                     <table className="todo-child">
                         <tbody>
                             <tr>
@@ -68,7 +121,7 @@ class ListTodo extends React.Component {
                             }
                         </tbody>
                     </table>
-                </div>
+                </div> */}
 
             </div>
         )
---
+++ src/views/Todos/TableJobTodo.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { EditOutlined, DeleteOutlined, CheckOutlined } from '@ant-design/icons';
+import { Button, Tooltip, Input } from "antd";
+import { toast } from 'react-toastify';
+
+class TableJobTodo extends React.Component {
+
+    handleDeleteTodo = (todo) => {
+        this.props.DeleteTodo(todo)
+        toast.success('Delete successfully!')
+    }
+    handleEditTodo = (todo) => {
+        // this.props.EditTodo(todo);
+        let { editTodobtn } = this.props;
+        if (editTodobtn.id) {
+            // Gọi hàm EditTodo trong file cha và truyền thông tin công việc cần chỉnh sửa
+            this.props.EditTodo(editTodobtn);
+
+            // Cập nhật trạng thái editTodobtn trong cha (nếu cần)
+            this.props.UpdateEditTodoBtn({});
+        }
+        this.props.UpdateEditTodoBtn(todo);
+
+
+
+    }
+    handleSaveTodo=(todo)=>{
+        this.props.EditTodo(todo);
+        this.props.UpdateEditTodoBtn({});
+        // toast.success('Update successfully!')
+
+        
+    }
+
+
+    handleOnChangeTitleTodo = (event) => {
+        let editTodoCopy = { ...this.props.editTodobtn }
+        editTodoCopy.title = event.target.value
+
+        // Cập nhật trạng thái editTodobtn trong cha (nếu cần)
+        this.props.UpdateEditTodoBtn(editTodoCopy);
+    }
+    handleOnChangeSalaryTodo = (event) => {
+        let editTodoCopy = { ...this.props.editTodobtn }
+        editTodoCopy.salary = event.target.value
+
+        // Cập nhật trạng thái editTodobtn trong cha (nếu cần)
+        this.props.UpdateEditTodoBtn(editTodoCopy);
+    }
+
+
+    render() {
+        let { ArrayJobTodo, editTodobtn } = this.props;
+        console.log('check edit btn ', editTodobtn);
+        let isEmptyObj = Object.keys(editTodobtn).length === 0;
+        console.log('check empty obj ', isEmptyObj);
+        return (
+
+            <div className='list-todo-content'>
+                <table className="todo-child">
+                    <tbody>
+                        <tr>
+                            <th>STT</th>
+                            <th>Title</th>
+                            <th>Salary</th>
+                            <th>Action</th>
+                        </tr>
+                        {ArrayJobTodo && ArrayJobTodo.length > 0 &&
+                            ArrayJobTodo.map((item, index) => {
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{index + 1}</td>
+                                        {isEmptyObj === true ?
+                                            <td>{item.title}</td>
+                                            :
+                                            <>
+                                                {editTodobtn.id === item.id ?
+                                                    <td><Input
+                                                        onChange={(event) => { this.handleOnChangeTitleTodo(event) }}
+                                                        value={editTodobtn.title} /></td>
+                                                    :
+                                                    <td>{item.title}</td>
+
+                                                }
+                                            </>
+                                        }
+                                        {isEmptyObj === true ?
+                                            <td>{item.salary}</td>
+                                            :
+                                            <>
+                                                {editTodobtn.id === item.id ?
+                                                    <td><Input
+                                                        onChange={(event) => { this.handleOnChangeSalaryTodo(event) }}
+                                                        value={editTodobtn.salary} /></td>
+                                                    :
+                                                    <td>{item.salary}</td>
+
+                                                }
+                                            </>
+                                        }
+                                        <td >
+                                            {isEmptyObj === false && editTodobtn.id === item.id ?
+                                                <Tooltip title="Lưu">
+                                                    <Button
+                                                        className="btn-action"
+                                                        shape="circle"
+                                                        icon={<CheckOutlined />}
+                                                    onClick={() => this.handleSaveTodo(item)}
+                                                    />
+                                                </Tooltip>
+                                                :
+
+                                                <Tooltip title="Sửa">
+                                                    <Button
+                                                        className="btn-action"
+                                                        shape="circle"
+                                                        icon={<EditOutlined />}
+                                                        onClick={() => this.handleEditTodo(item)}
+                                                    />
+                                                </Tooltip>
+                                            }
+                                            <Tooltip title="Xóa">
+                                                <Button
+                                                    shape="circle"
+                                                    icon={<DeleteOutlined />}
+                                                    onClick={() => this.handleDeleteTodo(item)}
+                                                />
+                                            </Tooltip>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+}
+
+
+export default TableJobTodo;

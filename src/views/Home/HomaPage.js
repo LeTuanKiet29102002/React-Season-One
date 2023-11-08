0367d552a8e8b70c16cf112ea9f4b7005b974429
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-08 23:55:54 +0700

    HOC-React

--- src/views/Home/HomaPage.js
+++ src/views/Home/HomaPage.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Color from '../HOC/Color';
 
 class HomePage extends React.Component {
-    render(){
+
+    render() {
         return (
-            <div>Hello Home page</div>
+            <div style={{fontWeight:700}}>
+                Hello Home page
+                    Go to TODO
+            </div>
         )
     }
 }
 
-export default HomePage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const HomePage = () => {
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//         const timeoutId = setTimeout(() => {
+//             navigate('/todo');
+            
+//         }, 3000);
+        
+
+//         // Hủy bỏ timeout nếu component bị unmount trước khi timeout hoàn thành
+//         return () =>{ clearTimeout(timeoutId)};
+//     }, [navigate]);
+
+//     const goToTodo = () => {
+
+//         navigate('/todo'); // Đảm bảo rằng đường dẫn bắt đầu bằng '/'
+//     };
+
+//     return (
+//         <div>
+//             <div>Hello Home page</div>
+//             <button onClick={goToTodo}>
+//                 Go to TODO
+//             </button>
+//         </div>
+//     );
+// };
+export default Color(HomePage);

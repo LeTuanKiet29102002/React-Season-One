84b32bab51fd8b489335da707862d0675cb2ea94
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-08 21:06:50 +0700

    link-navlink-router-react

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,13 +1,21 @@
 import React from "react";
 import './Nav.scss'
+import { Link, NavLink } from "react-router-dom";
 
 class Nav extends React.Component {
     render() {
         return (
+            // exact={true} if /
             <div className="topnav">
-                <a className="active" href="/home">Home</a>
-                <a href="/todo">Todo</a>
-                <a href="/about">About</a>
+                <NavLink className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                } to="/home">Home</NavLink>
+                <NavLink className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                } to="/todo">Todo</NavLink>
+                <NavLink className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                } to="/about">About</NavLink>
             </div>
         )
     }
